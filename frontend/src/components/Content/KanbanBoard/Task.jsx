b6042d28b097e59f7b81ab0018c3daaf05943d76
Hristian Tachev <hristian.tachev@gmail.com>
2022-11-18 01:57:43 +0200

    Finished the task icons animations (made everything responsive and smooth).

--- frontend/src/components/Content/KanbanBoard/Task.jsx
+++ frontend/src/components/Content/KanbanBoard/Task.jsx
@@ -12,38 +12,54 @@ import testAssigneeProfilePic from '../../../assets/test_profile_pic_2.jpg';
 class Task extends Component {
     state = {
         taskFilters: ["Backend", "Frontend", "Ivan", "Icovich"],
-        status: "Not Done"
+        status: "Task Done",
+        taskInfoContainerRef: React.createRef(),
+        taskIconsContainerRef: React.createRef(),
+        taskTypeContainerRef: React.createRef(),
+        taskPriorityContainerRef: React.createRef(),
+        statusLabelRef: React.createRef()
     };
 
     componentDidMount(){
-        let taskTypeContainer = document.querySelector(".task-type-info-container");
-        let taskPriorityContainer = document.querySelector(".task-priority-info-container");
-        taskTypeContainer.onmouseover = (event) => {
-            // taskTypeContainer.querySelector(".image-container img").classList.add("image-slide-animation");
-            taskTypeContainer.querySelector(".fading-effect").classList.add("text-slide-animation");
-            taskTypeContainer.querySelector(".image-container img").style.marginRight = Number(window.getComputedStyle(taskTypeContainer.querySelector(".text-container")).width.replace("px", "")) + 5 + "px";
+        let { taskInfoContainerRef, taskIconsContainerRef, taskTypeContainerRef, taskPriorityContainerRef, statusLabelRef } = this.state;
+        let taskInfoContainer = taskInfoContainerRef.current;
+        let taskIconsContainer = taskIconsContainerRef.current;
+        let taskTypeContainer = taskTypeContainerRef.current;
+        let taskPriorityContainer = taskPriorityContainerRef.current;
+        let statusLabel = statusLabelRef.current;
+
+        let taskInfoContainerWidth = Number(window.getComputedStyle(taskInfoContainer).width.replace("px", ""));
+        let taskIconsContainerWidth = Number(window.getComputedStyle(taskIconsContainer).width.replace("px", ""));
+        let statusLabelWidth = Number(window.getComputedStyle(statusLabel).width.replace("px", ""));
+
+        statusLabel.style.maxWidth = (taskInfoContainerWidth - taskIconsContainerWidth - 10) + "px";
+
+        function onMouseOver (event, element) {
+            let imgMarginRight = Number(window.getComputedStyle(element.querySelector(".text-container")).width.replace("px", "")) + 5 + "px";
+            if((statusLabelWidth + taskIconsContainerWidth + Number(imgMarginRight.replace("px", ""))) > taskInfoContainerWidth){
+                statusLabel.style.opacity = "0.5";
+                statusLabel.style.filter = "blur(4px)";
+            }
+
+            element.querySelector(".fading-effect").classList.add("text-slide-animation");
+            element.querySelector(".image-container img").style.marginRight = imgMarginRight;
         };
-        taskTypeContainer.onmouseout = (event) => {
-            // taskTypeContainer.querySelector(".image-container img").classList.remove("image-slide-animation");
-            taskTypeContainer.querySelector(".fading-effect").classList.remove("text-slide-animation");
-            taskTypeContainer.querySelector(".image-container img").style.marginRight = "0px";
-        };
+        function onMouseOut (event, element) {
+            statusLabel.style.opacity = "1";
+            statusLabel.style.filter = "blur(0px)";
+            element.querySelector(".fading-effect").classList.remove("text-slide-animation");
+            element.querySelector(".image-container img").style.marginRight = "0px";
+        }
+
+        taskTypeContainer.onmouseover = (event) => { onMouseOver(event, taskTypeContainer) };
+        taskTypeContainer.onmouseout = (event) => { onMouseOut(event, taskTypeContainer) };
         
-        taskPriorityContainer.onmouseover = (event) => {
-            //taskPriorityContainer.querySelector(".image-container img").classList.add("image-slide-animation");
-            taskPriorityContainer.querySelector(".fading-effect").classList.add("text-slide-animation");
-            taskPriorityContainer.querySelector(".image-container img").style.marginRight = Number(window.getComputedStyle(taskPriorityContainer.querySelector(".text-container")).width.replace("px", "")) + 5 + "px";
-        };
-        taskPriorityContainer.onmouseout = (event) => {
-            //taskPriorityContainer.querySelector(".image-container img").classList.remove("image-slide-animation");
-            taskPriorityContainer.querySelector(".fading-effect").classList.remove("text-slide-animation");
-            taskPriorityContainer.querySelector(".image-container img").style.marginRight = "0px";
-        };
+        taskPriorityContainer.onmouseover = (event) => { onMouseOver(event, taskPriorityContainer) };
+        taskPriorityContainer.onmouseout = (event) => { onMouseOut(event, taskPriorityContainer) };
     }
 
     renderTaskFilters() {
         let { taskFilters } = this.state;
-
         return (
             <div className='flex'>
                 {taskFilters.map(filter => { return( <label className='task-filter'>{filter}</label> ) })}
@@ -52,7 +68,7 @@ class Task extends Component {
     }
 
     render() {
-        let { status } = this.state;
+        let { status, taskInfoContainerRef, taskIconsContainerRef, taskTypeContainerRef, taskPriorityContainerRef, statusLabelRef } = this.state;
 
         return (
             <div className='task flex column'>
@@ -72,24 +88,12 @@ class Task extends Component {
 
                 <div className='task-content flex column'>
 
-                    {/* <div className='separator width-100-percent' style={{ margin: "0.2rem auto 1rem auto" }}/> */}
+                    <div ref={taskInfoContainerRef} className='task-info-icons-container flex justify-space-between'>
+                        <label ref={statusLabelRef} className="task-status">Status: {status}</label>
 
-                    {/* <div className='task-associated-people-container flex justify-space-between'>
-                        <div className='flex align-center justify-center'>
-                            <label>Creator:</label>
-                            <img src={testCreatorProfilePic}/>
-                        </div>
-                        <div className='flex align-center justify-center'>
-                            <label>Assignee:</label>
-                            <img src={testAssigneeProfilePic}/>
-                        </div>
-                    </div> */}
+                        <div ref={taskIconsContainerRef} className='task-info-icons-animation-container flex'>
 
-                    <div className='task-info-icons-container flex justify-space-between'>
-                        <label style={{ marginRight: "auto" }}>Status: {status}</label>
-
-                        <div className='task-info-icons-animation-container flex'>
-                            <div className='task-type-info-container flex'>
+                            <div ref={taskTypeContainerRef} className='task-type-info-container flex'>
                                 <div className="image-container">
                                     <img src={taskIcon}/>
                                 </div>
@@ -101,7 +105,7 @@ class Task extends Component {
                         
                             <div className='separator-vertical' style={{ zIndex: "5" }} />
 
-                            <div className='task-priority-info-container flex'>
+                            <div ref={taskPriorityContainerRef} className='task-priority-info-container flex'>
                                 <div className="image-container">
                                     <img src={highPriority}/>
                                 </div>
