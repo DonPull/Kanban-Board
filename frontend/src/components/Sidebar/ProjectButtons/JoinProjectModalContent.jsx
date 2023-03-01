36ee0d2de596db6e051d0fe07f82aa7c96fc8859
Hristian Tachev <hristian.tachev@gmail.com>
2023-03-01 23:48:38 +0200

    Added JoinProjectModal input field clear on modal close.

--- frontend/src/components/Sidebar/ProjectButtons/JoinProjectModalContent.jsx
+++ frontend/src/components/Sidebar/ProjectButtons/JoinProjectModalContent.jsx
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom';
 import "./JoinProjectModalContent.css";
 
 class JoinProjectModalContent extends Component {
-    state = {  } 
+    state = {
+        codeInputFieldRef: React.createRef()
+    }
+
+    componentDidMount(){
+        this.props.modalOnCloseCallback(this.clearInput);
+    }
+
+    clearInput = () => {
+        this.state.codeInputFieldRef.current.value = "";
+    }
+    
     render() { 
         return (
             <div style={{ gap: "1rem" }} className='flex column align-center'>
                 <div className='flex join-project-input-pill'>
                     <label>Join Project</label>
                     <div style={{ margin: "0 0.5rem", height: "auto" }} className='separator-vertical' />
-                    <input type='text' placeholder='Enter Code...' />
+                    <input ref={this.state.codeInputFieldRef} type='text' placeholder='Enter Code...' />
                 </div>
                 <button style={{ width: "max-content" }} className='button'>Request Access</button>
             </div>
