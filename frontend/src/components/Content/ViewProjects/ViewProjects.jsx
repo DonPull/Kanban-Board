244cb0c52967afb97ad3d1c0fa6c2d47adbfcdbc
Hristian Tachev <hristian.tachev@gmail.com>
2023-03-04 03:53:05 +0200

    Finished the design of Project Cards and of View Projects page.

--- frontend/src/components/Content/ViewProjects/ViewProjects.jsx
+++ frontend/src/components/Content/ViewProjects/ViewProjects.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './ViewProjects.css';
 import collapseArrowIcon from '../../../assets/collapse_arrow.png';
 import ProjectCard from './ProjectCard';
+import addProjectIcon from '../../../assets/+_and_x_icon_v2.png';
 
 class ViewProjects extends Component {
     state = {
@@ -15,18 +16,30 @@ class ViewProjects extends Component {
         //let { myProjectsIsToggled, otherProjectsIsToggled } = this.state;
         let viewProjectsContainer = this.state.viewProjectsContainerRef.current;
 
+        viewProjectsContainer.querySelectorAll(".add-project-action-button")[0].onclick = () => { document.getElementById("create-project-btn").click(); }
+        viewProjectsContainer.querySelectorAll(".add-project-action-button")[1].onclick = () => { document.getElementById("join-project-btn").click(); }
+
         let myProjects = viewProjectsContainer.querySelectorAll(".project-cards-label-container")[0];
         myProjects.onclick = (event) => {
             let collapseIcon = myProjects.querySelector("img");
             console.log(collapseIcon);
             //console.log("myProjectsIsToggled: ", myProjectsIsToggled);
-            
+
+            let addProjectBtn = viewProjectsContainer.querySelectorAll(".add-project-action-button")[0];
             if(this.state.myProjectsIsToggled){
-                collapseIcon.style.transform = "rotate(0deg)";
-                viewProjectsContainer.querySelectorAll(".project-cards-container")[0].style.display = "";
-            }else{
                 collapseIcon.style.transform = "rotate(-180deg)";
+                viewProjectsContainer.querySelectorAll(".project-cards-container")[0].style.display = "";
+
+                addProjectBtn.style.transform = "translateX(0px)";
+                addProjectBtn.style.height = "";
+                addProjectBtn.querySelector("img").style.height = "";
+            }else{
+                collapseIcon.style.transform = "rotate(0deg)";
                 viewProjectsContainer.querySelectorAll(".project-cards-container")[0].style.display = "none";
+            
+                addProjectBtn.style.transform = "translateX(calc(-100% - 2rem))";
+                addProjectBtn.style.height = "0%";
+                addProjectBtn.querySelector("img").style.height = "0%";
             }
 
             //console.log("state before update myProjectsIsToggled: ", this.state.myProjectsIsToggled);
@@ -39,12 +52,21 @@ class ViewProjects extends Component {
             console.log(collapseIcon);
             //console.log("otherProjectsIsToggled: ", otherProjectsIsToggled);
 
+            let addProjectBtn = viewProjectsContainer.querySelectorAll(".add-project-action-button")[1]; 
             if(this.state.otherProjectsIsToggled){
-                collapseIcon.style.transform = "rotate(0deg)";
-                viewProjectsContainer.querySelectorAll(".project-cards-container")[1].style.display = "";
-            }else{
                 collapseIcon.style.transform = "rotate(-180deg)";
+                viewProjectsContainer.querySelectorAll(".project-cards-container")[1].style.display = "";
+
+                addProjectBtn.style.transform = "translateX(0px)";
+                addProjectBtn.style.height = "";
+                addProjectBtn.querySelector("img").style.height = "";
+            }else{
+                collapseIcon.style.transform = "rotate(0deg)";
                 viewProjectsContainer.querySelectorAll(".project-cards-container")[1].style.display = "none";
+
+                addProjectBtn.style.transform = "translateX(calc(-100% - 2rem))";
+                addProjectBtn.style.height = "0%";
+                addProjectBtn.querySelector("img").style.height = "0%";
             }
 
             this.setState({ otherProjectsIsToggled: !this.state.otherProjectsIsToggled });
@@ -57,26 +79,40 @@ class ViewProjects extends Component {
         return (
             <div ref={viewProjectsContainerRef} id="view-projects-container" className='flex column'>
                 <div className='flex column'>
-                    <div className='project-cards-label-container flex'>
-                        <label>My Projects</label>
-                        <div className='collapse-project-cards-icon-container'>
-                            <img src={collapseArrowIcon} />
+                    <div className='flex align-center' style={{ position: "relative", gap: "1rem" }}>
+                        <div className='project-cards-label-container flex'>
+                            <label>My Projects</label>
+                            <div className='collapse-project-cards-icon-container'>
+                                <img src={collapseArrowIcon} />
+                            </div>
+                        </div>
+                        <div className='add-project-action-button'>
+                            <img src={addProjectIcon}/>
                         </div>
                     </div>
                     <div className='project-cards-container flex'>
-                        <ProjectCard />
+                        <ProjectCard projectTitle="Online Ecommers Store" />
                         <ProjectCard />
                     </div>
                 </div>
 
                 <div className='flex column'>
-                    <div className='project-cards-label-container flex'>
-                        <label>Other Projects</label>
-                        <div className='collapse-project-cards-icon-container'>
-                            <img src={collapseArrowIcon} />
+                    <div className='flex align-center' style={{ position: "relative", gap: "1rem" }}>
+                        <div className='project-cards-label-container flex'>
+                            <label>Other Projects</label>
+                            <div className='collapse-project-cards-icon-container'>
+                                <img src={collapseArrowIcon} />
+                            </div>
+                        </div>
+                        <div className='add-project-action-button'>
+                            <img src={addProjectIcon}/>
                         </div>
                     </div>
                     <div className='project-cards-container flex'>
+                        <ProjectCard projectTitle="Kanban Board" />
+                        <ProjectCard projectTitle="Migration to different technology" />
+                        <ProjectCard projectTitle="Photography editing" />
+                        <ProjectCard />
                         <ProjectCard />
                         <ProjectCard />
                         <ProjectCard />
